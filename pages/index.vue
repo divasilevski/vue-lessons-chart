b499615b41583556375e7858b11eed23cb5c1524
divasilevski <divasilevski@gmail.com>
2020-02-26 16:02:08 +0300

    Day 3 (vuetify)

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,62 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn @click="message">
-        NEW MESSAGE
-      </v-btn>
+    <v-flex xs12 sm8>
+      <v-card min-width="400px">
+        <v-card-title>
+          Приветствую ПУТНИК...
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Ваше имя"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Комната"
+              required
+            ></v-text-field>
+
+            <v-btn :disabled="!valid" color="primary" @click="submit">
+              Войти
+            </v-btn>
+
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
+  layout: "empty",
   sockets: {
     connect: function() {
       console.log("socket connected");
     }
   },
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Введите имя",
+      v => (v && v.length <= 16) || "Имя не должно превышать 16 символов"
+    ],
+    room: "",
+    roomRules: [
+      v => !!v || "введите комнату"],
+  }),
   methods: {
-    message() {
-      this.$socket.emit("createMessage", {
-        text: "FROM CLIENT"
-      });
+    submit() {
+      if (this.$refs.form.validate()) {
+
+      }
     }
   }
 };
